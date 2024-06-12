424f05f8e531d9cedfcc8fb7dbc06d29b8de4e60
Susan Rahmawati <susanrahma98@gmail.com>
2024-06-12 13:50:14 +0200

    change file

--- src/Components/ColorForm/ColorForm.jsx
+++ src/Components/ColorForm/ColorForm.jsx
@@ -1,46 +1,50 @@
 import ColorInput from "../ColorInput/ColorInput.jsx";
-import "../ColorForm.css";
 
-
-export default function ColorForm({onSubmitColor, intialData = { role: "some color", hex: #12345, contastText: "#ffffff"},
-
+export default function ColorForm({
+  onSubmitColor,
+  initialData = { role: "some color", hex: "#12345", contrastText: "#ffffff" },
 }) {
-    function handleSubmit(event) {
-        event.preventDefault();
-        const formData = new FormData(event.target); /*Mengumpulkan form data, jika user submit form*/
-        const data = Object.fromEntries(formData); /*Konversi FormData Menjadi Objek JavaScript*/
-        onSubmitColor(data); /*Fungsi yang mengirimkan data dari form ke suatu tempat */
-
-    }
-    return (
-        <form className="" onSubmit={handleSubmit}> /*onSubmit adalah atribut, yang menentukan apa yang terjadi ketikan user submit*/
-
-        <label htmlFor="role">
-        <br /> */garis kosong */
-        <input
-        type="text"
-        id="role"
-        name="role"
-        defaultValue={initialData.role} /* atribut defaultValue digunakan utk mengatur nilai awal dari input, jadi nilai input menjadi role  */
-       />
-        </label>
-        <br /> 
-
-        <label htmlFor="hex">
-            Hex
-            <br />
-            <ColorInput id="hex" defaultValue={intialData.hex}/>
-        </label>
-        <br /> 
-
-        <label htmlFor="contrastText">
-            Contast Text
-            <br /> 
-            <ColorInput id="contrastText" defaultValue={initialData.contrastText}/>
-        </label>
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(
+      event.target
+    ); /*Mengumpulkan form data, jika user submit form*/
+    const data =
+      Object.fromEntries(
+        formData
+      ); /*Konversi FormData Menjadi Objek JavaScript*/
+    onSubmitColor(
+      data
+    ); /*Fungsi yang mengirimkan data dari form ke suatu tempat */
+  }
+  return (
+    <form className="" onSubmit={handleSubmit}>
+      {" "}
+      <label htmlFor="role">
         <br />
-        <button>Add Color</button>
-        </form>
-    )
-    
+        <input
+          type="text"
+          id="role"
+          name="role"
+          defaultValue={
+            initialData.role
+          } /* atribut defaultValue digunakan utk mengatur nilai awal dari input, jadi nilai input menjadi role  */
+        />
+      </label>
+      <br />
+      <label htmlFor="hex">
+        Hex
+        <br />
+        <ColorInput id="hex" defaultValue={initialData.hex} />
+      </label>
+      <br />
+      <label htmlFor="contrastText">
+        Contast Text
+        <br />
+        <ColorInput id="contrastText" defaultValue={initialData.contrastText} />
+      </label>
+      <br />
+      <button>Add Color</button>
+    </form>
+  );
 }
